496da456d0fa1864a2df6c7bb65332e1d47d0cdd
muhammadilhamarifin <104124455+muhammadilhamarifin@users.noreply.github.com>
2022-04-21 13:19:07 +0700

    Add files via upload

---
+++ Muhammad Ilham Arifin - 191011402143 - UTS Kecerdasan buatan.py
@@ -0,0 +1,21 @@
+#Muhammad Ilham Arifin
+#191011402143
+#Looping
+
+print("Muhammad Ilham Arifin")
+print("191011402143")
+
+#Contoh 1
+for x in range(5):
+	print("Ayo Kita Semangat Uts")
+
+#Contoh 2
+for x in range(6):
+    print(x)
+
+#Contoh 3
+i = 5
+while(i<10):
+    #Do something
+    i = i + i
+    print(i)
